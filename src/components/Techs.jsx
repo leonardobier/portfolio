f2fae3d1e36973af037da259d3b3d279063f18d3
Leonardo Bier <leobier182@gmail.com>
2026-04-22 14:03:12 +0000

    add

--- src/components/Techs.jsx
+++ src/components/Techs.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaPython, FaGitAlt } from "react-icons/fa";
-import { 
-  SiVite, 
-  SiGithub, 
-  SiMysql, 
-  SiMicrosoft,   // usado para representar C#
-  SiDotnet,      // ícone oficial do .NET
-  SiVisualstudio, 
-  SiVisualstudiocode 
-} from "react-icons/si";
+import { SiVite, SiGithub, SiMysql } from "react-icons/si";
+import { Icon } from "@iconify/react";
 
 const Techs = () => {
   return (
@@ -16,56 +9,31 @@ const Techs = () => {
       <h2>Tecnologias</h2>
       <p>Essas são algumas das ferramentas e linguagens que utilizo no meu dia a dia:</p>
       <div className="tech-grid">
+        <div className="tech-card"><FaHtml5 className="tech-icon html" /><span>HTML5</span></div>
+        <div className="tech-card"><FaCss3Alt className="tech-icon css" /><span>CSS3</span></div>
+        <div className="tech-card"><FaJs className="tech-icon js" /><span>JavaScript</span></div>
+        <div className="tech-card"><FaReact className="tech-icon react" /><span>React</span></div>
+        <div className="tech-card"><SiVite className="tech-icon vite" /><span>Vite</span></div>
+        <div className="tech-card"><FaPython className="tech-icon python" /><span>Python</span></div>
+        <div className="tech-card"><SiMysql className="tech-icon mysql" /><span>MySQL</span></div>
+        <div className="tech-card"><FaGitAlt className="tech-icon git" /><span>Git</span></div>
+        <div className="tech-card"><SiGithub className="tech-icon github" /><span>GitHub</span></div>
+
+        {/* Usando Iconify para os ícones que faltavam */}
         <div className="tech-card">
-          <FaHtml5 className="tech-icon html" />
-          <span>HTML5</span>
-        </div>
-        <div className="tech-card">
-          <FaCss3Alt className="tech-icon css" />
-          <span>CSS3</span>
-        </div>
-        <div className="tech-card">
-          <FaJs className="tech-icon js" />
-          <span>JavaScript</span>
-        </div>
-        <div className="tech-card">
-          <FaReact className="tech-icon react" />
-          <span>React</span>
-        </div>
-        <div className="tech-card">
-          <SiVite className="tech-icon vite" />
-          <span>Vite</span>
-        </div>
-        <div className="tech-card">
-          <FaPython className="tech-icon python" />
-          <span>Python</span>
-        </div>
-        <div className="tech-card">
-          <SiMysql className="tech-icon mysql" />
-          <span>MySQL</span>
-        </div>
-        <div className="tech-card">
-          <FaGitAlt className="tech-icon git" />
-          <span>Git</span>
-        </div>
-        <div className="tech-card">
-          <SiGithub className="tech-icon github" />
-          <span>GitHub</span>
-        </div>
-        <div className="tech-card">
-          <SiMicrosoft className="tech-icon microsoft" />
+          <Icon icon="simple-icons:microsoft" className="tech-icon microsoft" />
           <span>C#</span>
         </div>
         <div className="tech-card">
-          <SiDotnet className="tech-icon dotnet" />
+          <Icon icon="simple-icons:dotnet" className="tech-icon dotnet" />
           <span>.NET</span>
         </div>
         <div className="tech-card">
-          <SiVisualstudio className="tech-icon visualstudio" />
+          <Icon icon="simple-icons:visualstudio" className="tech-icon visualstudio" />
           <span>Visual Studio</span>
         </div>
         <div className="tech-card">
-          <SiVisualstudiocode className="tech-icon vscode" />
+          <Icon icon="simple-icons:visualstudiocode" className="tech-icon vscode" />
           <span>Visual Studio Code</span>
         </div>
       </div>
